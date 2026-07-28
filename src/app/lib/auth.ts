840b795d1dffa71926c7cae349250dda61f4680b
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-29 00:58:51 +0600

    google authentication id add

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -15,7 +15,7 @@ export const auth = betterAuth({
     socialProviders: {
         github: {
             clientId: process.env.GITHUB_CLIENT_ID as string,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         },
     },
 
