cd3ce89878316d92fea88aec63040fb93b5b1d1b
tfshorifulislam <tfshorifulislam@gmail.com>
2026-07-29 00:52:59 +0600

    better auth setup done

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,22 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(MONGODB_CONNECTED);
+const db = client.db('zico');
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        // Optional: if you don't provide a client, database transactions won't be enabled.
+        client
+    }),
+
+
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+    },
+
+});
